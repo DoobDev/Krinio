886f7a71101107b7f6c90f0eeaa41b812e284a3c
mmattbtw <matt@mmatt.net>
2020-11-08 16:12:18 -0600

    game done

--- data/db/build.sql
+++ data/db/build.sql
@@ -3,9 +3,17 @@ CREATE TABLE IF NOT EXISTS guilds(
     Prefix text DEFAULT "k!",
     LogChannel text,
     CodeChannel text,
-    ReadyUpChannel text
+    ReadyUpChannel text,
+    Ping text DEFAULT "@here"
 );
 
 CREATE TABLE IF NOT EXISTS users(
     UserID integer PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS games(
+    MessageID integer,
+    GuildID integer,
+    ReactionCount integer DEFAULT 1,
+    PRIMARY KEY("MessageID", "GuildID")
 )
--- lib/cogs/game.py
+++ lib/cogs/game.py
@@ -3,7 +3,31 @@ from discord import Embed
 from discord.ext.commands import Cog
 from discord.ext.commands import command, cooldown, BucketType
 
+from discord.ext import menus
+
+from discord.utils import get
+
 from ..db import db # pylint: disable=relative-beyond-top-level
+
+class GameMenu(menus.Menu):
+    async def send_initial_message(self, ctx, channel):
+        embed = Embed(title="Game Starting", colour=ctx.author.colour)
+        embed.set_footer(text=f"Started By: {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
+        embed.set_author(name="✅ - Ready Up | ⏹ - Clear Reactions")
+        return await channel.send(embed=embed)
+
+    @menus.button('✅')
+    async def on_checkmark(self, payload):
+        channel = self.bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        reaction = get(message.reactions, emoji=payload.emoji.name)
+        await self.message.edit(content=f"{reaction.count - 1}/10 ready.")
+        if reaction and reaction.count >= 10:
+            await message.clear_reactions()
+
+    @menus.button('\N{BLACK SQUARE FOR STOP}\ufe0f')
+    async def on_stop(self, payload):
+        self.stop()
 
 class Game(Cog):
     def __init__(self, bot):
@@ -22,6 +46,17 @@ class Game(Cog):
         code_channel = await self.bot.fetch_channel(db.field("SELECT CodeChannel FROM guilds WHERE GuildID = ?", ctx.guild.id))
         await code_channel.send(embed=embed)
         await ctx.send(f"Code sent {ctx.author.mention}", delete_after=10)
+
+    @command(name="startgame", aliases=["start"], brief="Lets the user start a game.")
+    @cooldown(1, 4, BucketType.user)
+    async def start_game(self, ctx):
+        """Starts a game that lets people ready up!"""
+        ping = db.record("SELECT Ping from guilds WHERE GuildID = ?", ctx.guild.id)
+
+        m = GameMenu(clear_reactions_after=True)
+        await m.start(ctx)
+
+        await ctx.send(f"{ping[0]} Game is starting!~")
 
     @Cog.listener()
     async def on_ready(self):
